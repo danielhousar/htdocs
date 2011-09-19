d08c98e20374a60d6bcd0aaf4627dbe653d57908
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-09-19 14:06:35 +0200

    [7] contacts.php: removed some echo() calls, moved their output to less count of echo()

--- contacts.php
+++ contacts.php
@@ -2,22 +2,14 @@
 switch ($lng) {
 case "en":
 	echo "<header> &nbsp; Contacts</header>";
-	echo "<p>";
-	echo "Facebook: <a href=\"http://www.facebook.com/daniel.housar\">www.facebook.com/daniel.housar</a>";
-	echo "</p><p>";
-	echo "e-mail: danielhousar@gmail.com";
-	echo "</p><p>";
-	echo "phone: (+420) 776 238 702";
-	echo "</p>";
+	echo "<p>Facebook: <a href=\"http://www.facebook.com/daniel.housar\">www.facebook.com/daniel.housar</a></p>";
+	echo "<p>e-mail: danielhousar@gmail.com</p>";
+	echo "<p>phone: (+420) 776 238 702</p>";
 	break;
 default:
 	echo "<header> &nbsp; Kontakty</header>";
-	echo "<p>";
-	echo "Facebook: <a href=\"http://www.facebook.com/daniel.housar\" target=\"new\">www.facebook.com/daniel.housar</a>";
-	echo "</p><p>";
-	echo "e-mail: danielhousar@gmail.com";
-	echo "</p><p>";
-	echo "telefon: (+420) 776 238 702";
-	echo "</p>";
+	echo "<p>Facebook: <a href=\"http://www.facebook.com/daniel.housar\" target=\"new\">www.facebook.com/daniel.housar</a></p>";
+	echo "<p>e-mail: danielhousar@gmail.com</p>";
+	echo "<p>telefon: (+420) 776 238 702</p>";
 }
 ?>
